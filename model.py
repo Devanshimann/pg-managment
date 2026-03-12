fe2f76921f4f8e0f80d4fdccf2a5cf1f1b7787a1
Devanshimann <itsdevanshi30@gmail.com>
2026-03-12 22:46:04 +0530

    done

--- model.py
+++ model.py
@@ -43,7 +43,42 @@ plt.title("Rating vs Rent")
 # plt.show()
 
 cheapest = df.groupby("area")["rent"].mean().sort_values().head(5)
-
+df["area_avg_rent"] = df.groupby("area")["rent"].transform("mean")
 print("Most Affordable Areas")
 print(cheapest)
+df["wifi"] = df["wifi"].map({"yes":1,"no":0})
+df["ac"] = df["ac"].map({"yes":1,"no":0})
+df["laundry"] = df["laundry"].map({"yes":1,"no":0})
+df["amenity_score"] = df["wifi"] + df["ac"] + df["laundry"]
+from sklearn.preprocessing import LabelEncoder
 
+encoder = LabelEncoder()
+
+df["room_type"] = encoder.fit_transform(df["room_type"])
+df["gender"] = encoder.fit_transform(df["gender"])
+df = pd.get_dummies(df, columns=["area"], drop_first=True)
+x=df.drop('rent',axis=1)
+y=df['rent']
+from sklearn.model_selection import train_test_split
+x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2,random_state=42)
+from sklearn.linear_model import LinearRegression
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
+
+from sklearn.metrics import mean_absolute_error, r2_score
+# model={
+#     "linear":LinearRegression(),
+#     "forest":RandomForestRegressor(),
+#     "tree":DecisionTreeRegressor()
+# }
+# for name,models in model.items():
+#     models.fit(x_train,y_train)
+#     pred=models.predict(x_test)
+#     mse=mean_absolute_error(y_test,pred)
+#     r2=r2_score(y_test,pred)
+#     print(f"mse is {mse} r2 is {r2}")
+model=RandomForestRegressor()
+model.fit(x_train,y_train)
+pred=model.predict(x_test)
+mse=mean_absolute_error(y_test,pred)
+print(mse)
